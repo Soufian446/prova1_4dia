681b377dd092f3adfb4e5d26011069dfe848ebba
soufian446 <hammsoufian@gmail.com>
2026-03-05 11:41:02 +0100

    Secondo commit

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+let luoghiFirenze = [
+  {
+    nome: "Duomo di Firenze (Cattedrale di Santa Maria del Fiore)",
+    latitudine: 43.7731,
+    longitudine: 11.2560
+  },
+  {
+    nome: "Galleria degli Uffizi",
+    latitudine: 43.7687,
+    longitudine: 11.2550
+  },
+  {
+    nome: "Palazzo Vecchio",
+    latitudine: 43.7696,
+    longitudine: 11.2563
+  },
+  {
+    nome: "Ponte Vecchio",
+    latitudine: 43.7680,
+    longitudine: 11.2531
+  },
+  {
+    nome: "Galleria dell'Accademia",
+    latitudine: 43.7769,
+    longitudine: 11.2586
+  }
+];
+
+
+
 alert("alert");
